fdcb1a0d3176ab5c08ffe8fe3f66d7a0bf555f8d
Ben Redden <benjamminredden@gmail.com>
2018-03-18 12:21:52 -0400

    adding download link back

--- src/Components/BallEditor/BallEditor.js
+++ src/Components/BallEditor/BallEditor.js
@@ -48,37 +48,37 @@ const padNumber = (number, length) => {
   return str;
 }
 
-// const createDownload = (data, filename, mime) => {
-//   var blob = new Blob([data], {type: mime || 'application/octet-stream'});
+const createDownload = (data, filename, mime) => {
+  var blob = new Blob([data], {type: mime || 'application/octet-stream'});
 
-//   if (typeof window.navigator.msSaveBlob !== 'undefined') {
-//       // IE workaround for "HTML7007: One or more blob URLs were 
-//       // revoked by closing the blob for which they were created. 
-//       // These URLs will no longer resolve as the data backing 
-//       // the URL has been freed."
-//       window.navigator.msSaveBlob(blob, filename);
-//   }
-//   else {
-//       var blobURL = window.URL.createObjectURL(blob);
-//       var tempLink = document.createElement('a');
-//       tempLink.style.display = 'none';
-//       tempLink.href = blobURL;
-//       tempLink.setAttribute('download', filename); 
+  if (typeof window.navigator.msSaveBlob !== 'undefined') {
+      // IE workaround for "HTML7007: One or more blob URLs were 
+      // revoked by closing the blob for which they were created. 
+      // These URLs will no longer resolve as the data backing 
+      // the URL has been freed."
+      window.navigator.msSaveBlob(blob, filename);
+  }
+  else {
+      var blobURL = window.URL.createObjectURL(blob);
+      var tempLink = document.createElement('a');
+      tempLink.style.display = 'none';
+      tempLink.href = blobURL;
+      tempLink.setAttribute('download', filename); 
       
-//       // Safari thinks _blank anchor are pop ups. We only want to set _blank
-//       // target if the browser does not support the HTML5 download attribute.
-//       // This allows you to download files in desktop safari if pop up blocking 
-//       // is enabled.
-//       if (typeof tempLink.download === 'undefined') {
-//           tempLink.setAttribute('target', '_blank');
-//       }
+      // Safari thinks _blank anchor are pop ups. We only want to set _blank
+      // target if the browser does not support the HTML5 download attribute.
+      // This allows you to download files in desktop safari if pop up blocking 
+      // is enabled.
+      if (typeof tempLink.download === 'undefined') {
+          tempLink.setAttribute('target', '_blank');
+      }
       
-//       document.body.appendChild(tempLink);
-//       tempLink.click();
-//       document.body.removeChild(tempLink);
-//       window.URL.revokeObjectURL(blobURL);
-//   }
-// }
+      document.body.appendChild(tempLink);
+      tempLink.click();
+      document.body.removeChild(tempLink);
+      window.URL.revokeObjectURL(blobURL);
+  }
+}
 
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
@@ -224,7 +224,8 @@ xmlObj.southSequences.sequences.map((seq, index) => {
 });
 xmlFile += `</PROGRAMS_SOUTH>\n</SPEEVERS_LIGHT_DATA>`;
     
-    return this.setState({fileGenerationModalOpen: true, xmlFile: xmlFile});
+    return createDownload(xmlFile, 'speevers.xml', 'text/xml');
+    // return this.setState({fileGenerationModalOpen: true, xmlFile: xmlFile});
   }
 
   /* Settings Methods */
